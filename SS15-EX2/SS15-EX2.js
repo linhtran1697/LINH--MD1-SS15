bed73a10362b16fc1b81ef789119a91c93d26d5b
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-06-08 19:01:55 +0900

    Add files via upload

---
+++ SS15-EX2/SS15-EX2.js
@@ -0,0 +1,6 @@
+//Bài 1: Viết một chương trình để in các ký tự trong một mảng theo thứ tự đảo ngược. 
+// Ví dụ, nếu một mảng chứa các ký tự [ ‘c’, ‘s’, ‘c’, ‘2’, ‘6’, ‘1’ ] 
+// sau khi thực hiện đảo ngược sẽ là “261csc”
+
+let arrayB = ['i', 'a', 'g', 'h', 'n', 'i', 'x', 'h', 'n', 'i', 'l'];
+console.log(arrayB.reverse().join(''));
